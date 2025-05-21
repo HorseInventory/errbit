d3cac86b20a01aeb6d8bf6438249bebb3b106c60
Marc Beaupré <hrdwdmrbl@gmail.com>
2025-05-21 16:39:19 -0400

    Feat: Delete excess Notices and Errs

--- app/interactors/problem_merge.rb
+++ app/interactors/problem_merge.rb
@@ -1,6 +1,8 @@
 require 'problem_destroy'
 
 class ProblemMerge
+  MAX_RECENT_NOTICES = 100
+
   def initialize(*problems)
     problems = problems.flatten.uniq
     @merged_problem = problems[0]
@@ -16,7 +18,31 @@ class ProblemMerge
       problem.reload # deference all associate objet to avoid delete him after
       ProblemDestroy.execute(problem)
     end
+    
+    # Keep only the MAX_RECENT_NOTICES most recent notices and related Errs
+    trim_old_notices_and_errs
+    
     merged_problem.recache
     merged_problem
+  end
+
+  private
+
+  def trim_old_notices_and_errs
+    # Get count of all notices
+    total_count = merged_problem.notices.count
+    
+    if total_count > MAX_RECENT_NOTICES
+      # Get notices to keep (100 most recent)
+      notices_to_keep = merged_problem.notices.reverse_ordered.limit(MAX_RECENT_NOTICES)
+      notice_ids_to_keep = notices_to_keep.pluck(:id)
+
+      # Delete notices that belong to errs we're keeping but aren't in our keep list
+      merged_problem.notices.where(:id.nin => notice_ids_to_keep).delete
+
+      # Delete errs and their associated notices we don't want to keep
+      err_ids_to_keep = notices_to_keep.pluck(:err_id).uniq
+      merged_problem.errs.where(:id.nin => err_ids_to_keep).delete
+    end
   end
 end
